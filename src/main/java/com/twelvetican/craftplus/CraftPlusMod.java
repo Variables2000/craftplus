5fdea09eda5f75f0ef181573d8ebe96cd8cbf254
Rhelvetican <bionicvnb@gmail.com>
2023-02-23 09:59:16 +0700

    Adding 2 more recipes.

--- src/main/java/com/twelvetican/craftplus/CraftPlusMod.java
+++ src/main/java/com/twelvetican/craftplus/CraftPlusMod.java
@@ -6,7 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CraftPlusMod implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("craftplus");
+	public static final String MOD_ID = "craftplus";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
